980ece5b4f54a8e2b74aaa06cd2496dad9749430
pleaseshutup <pleaseshutup@gmail.com>
2016-10-28 07:31:55 -0700

    underdom now starts all methods with _ and all underdom methods should always reutrn the html node for chaining

--- underdom.js
+++ underdom.js
@@ -1,32 +1,32 @@
-Node.prototype.css = function(css){
+Node.prototype._css = function(css){
 	for(var k in css){ this.style.setProperty(k, css[k]); }
 	return this;
 }
-Node.prototype.attr = function(attr){
+Node.prototype._attr = function(attr){
 	for(var k in attr){ !attr[k] && attr[k] !== 0 ? this.removeAttribute(k) : typeof attr[k] === 'boolean' ? this[k] = attr[k] : this.setAttribute(k, attr[k]); }
 	return this;
 }
-Node.prototype.text = function(text){
+Node.prototype._text = function(text){
 	this.textContent = text || '';
 	return this;
 }
-Node.prototype.html = function(html){
+Node.prototype._html = function(html){
 	this.innerHTML = html || '';
 	return this;
 }
-Node.prototype.appendTo = function(target){
+Node.prototype._appendTo = function(target){
 	return target.appendChild(this);
 }
-Node.prototype.append = function(el){
+Node.prototype._append = function(el){
 	this.appendChild(el);
 	return this;
 }
-Node.prototype.on = function(ev, fn){
+Node.prototype._on = function(ev, fn){
 	this.addEventListener(ev, fn);
 	return this;
 }
 NodeList.prototype.__proto__ = Array.prototype;
-var underdom_methods = ['css', 'attr', 'text', 'html', 'appendTo', 'append', 'on'];
+var underdom_methods = ['_css', '_attr', '_text', '_html', ')appendTo', ')append', '_on'];
 underdom_methods.forEach(function(method){
 	NodeList.prototype[method] = HTMLCollection.prototype[method] = function(arg1, arg2){
 		for (var i = this.length - 1; i >= 0; i--) {
@@ -35,3 +35,4 @@ underdom_methods.forEach(function(method){
 		return this;
 	}
 });
+
